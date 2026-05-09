a3cd85c91af2b0066ea04e83c1de9159253b7449
King-Volta <pramudyarangga919@gmail.com>
2026-05-09 17:32:14 +0700

    Update initial condition for hyperchaotic system
    Changed the initial condition of x6 from 0.1 to 0.01.

--- hyperchaotic 6d.py
+++ hyperchaotic 6d.py
@@ -21,7 +21,7 @@ x5 = np.zeros(N)
 x6 = np.zeros(N)
 
 # initial condition
-x1[0], x2[0], x3[0], x4[0], x5[0], x6[0] = 0.1, 0.1, 0.1, 0.1, 0.1, 0.1
+x1[0], x2[0], x3[0], x4[0], x5[0], x6[0] = 0.1, 0.1, 0.1, 0.1, 0.1, 0.01
 
 # Hyperchaotic 6D
 def deriv(x):
